362ae98a92aeef54196939276fa4312f49011a66
John Tordoff <>
2023-10-24 22:40:22 +0800

    initial commit

---
+++ tests/test_base.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from .factories import UserFactory
+
+
+class TestTestCase(TestCase):
+    def test_tests(self):
+        """Simple base test to test test infrastructure"""
+        pass
+
+    def test_model(self):
+        """Simple base test to test test infrastructure"""
+        user = UserFactory(username="Jalen Hurts")
+        user.save()
+        assert user.username == "Jalen Hurts"
